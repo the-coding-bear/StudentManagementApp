dfc6cdcbd91dfc3d05d7dadfe97c3b9f56600b96
Akanksha <akanksha060801@gmail.com>
2024-05-15 09:46:11 +0530

    Add main application logic for managing students with options to add, delete, display, and exit.

--- src/com/student/manage/StudentDao.java
+++ src/com/student/manage/StudentDao.java
@@ -2,6 +2,8 @@ package com.student.manage;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class StudentDao {
     public static boolean insertStudentToDB(Student st){
@@ -26,5 +28,59 @@ public class StudentDao {
             e.printStackTrace();
         }
         return f;
+    }
+
+    public static boolean deleteStudent(int userId) {
+
+        boolean f = false;
+        try {
+
+            //jdbc code..
+            Connection con = CP.createC();
+            String q ="delete from students where sid = ?";
+            //PreparedStatement
+            PreparedStatement pstmt = con.prepareStatement(q);
+            //set the value of parameter
+            pstmt.setInt(1, userId);
+
+            // execute..
+            pstmt.executeUpdate();
+            f=true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    public static void showAllStudent() {
+
+        boolean f = false;
+        try {
+
+            //jdbc code..
+            Connection con = CP.createC();
+            String q ="select * from students;";
+            Statement stmt = con.createStatement();
+
+            ResultSet set = stmt.executeQuery(q);
+
+            while (set.next()){
+                int id = set.getInt(1);
+                String name = set.getString(2);
+                String phone = set.getString(3);
+                String city = set.getString("scity");
+
+                System.out.println("ID : "+id);
+                System.out.println("Name : "+name);
+                System.out.println("Phone : "+phone);
+                System.out.println("City :"+city);
+                System.out.println("*---------*---------*");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
